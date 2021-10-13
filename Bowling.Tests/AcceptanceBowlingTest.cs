892c867e577217482c101f7d3b7ff0b2cf19dd64
Alonso Bustos <alonsobh@gmail.com>
2021-10-13 18:06:42 -0500

    Add Acceptance Test

---
+++ Bowling.Tests/AcceptanceBowlingTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bowling.Tests
+{
+    [Ignore]
+    [TestClass]
+    public class AcceptanceBowlingTest
+    {
+        // _____ _____ _____ _____ _____ _____ _____ _____ _____ _______
+        //| 1 |4| 4 |5| 6 |/| 5 |/|   |X| 0 |1| 7 |/| 6 |/|   |X| 2 |/|6|
+        //|  5  |  14 |  29 |  49 |  60 |  61 |  77 |  97 | 117 |  133  |
+
+        [TestMethod]
+        public void MyTestMethod()
+        {
+            var game = GetNewGame();
+            RollFrame(1, 4, game);
+            RollFrame(4, 5, game);
+            RollSpare(6, game);
+            RollSpare(5, game);
+            RollStrike(game);
+            RollFrame(0, 1, game);
+            RollSpare(7, game);
+            RollSpare(6, game);
+            RollStrike(game);
+            RollSpare(2, game);
+            Roll(6, game);
+
+
+            Assert.AreEqual(133, GetScore(game));
+        }
+
+        private int GetScore(object game) 
+            => throw new System.NotImplementedException();
+
+        private object GetNewGame()
+            => throw new System.NotImplementedException();
+
+        private void RollFrame(int first, int second, object game)
+        {
+            Roll(first, game);
+            Roll(second, game);
+        }
+
+        private void RollSpare(int pins, object game)
+        {
+            Roll(pins, game);
+            Roll(10 - pins, game);
+        }
+
+        private void RollStrike(object game)
+            => Roll(10, game);
+
+        private void Roll(int pins, object game)
+            => throw new System.NotImplementedException();
+    }
+}
